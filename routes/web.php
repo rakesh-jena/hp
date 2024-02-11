4880c3c73239363d95a5c6eec2bc1d91448104ee
Rakesh <rakeshranjanjena7@gmail.com>
2024-02-11 11:22:25 +0530

    Assets added for admin

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('login', function () {
+    return view('admin.auth.login');
+})->name('login');
+Route::get('dashboard', function () {
+    return view('admin.dashboard');
+})->name('admin.dashboard');
